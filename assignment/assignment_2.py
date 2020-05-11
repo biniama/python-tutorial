0e3e347162f0cd4bcca13e9d518915a3c2917bdb
Biniam Asnake <biniam.asnake@kaufda.de>
2020-05-11 13:14:25 +0200

    Added assignment 2

---
+++ assignment/assignment_2.py
@@ -0,0 +1,20 @@
+# Write a program that accepts an unlimited number of students that are taking
+# ‘Python Programming’ class. The user tells you how many students are there.
+#
+# A student has an ID, name, department and exam result (out of 100).
+#
+# Calculate the Grade based on the exam result as follows.
+#   A > 85
+#   B > 70
+#   C > 50
+#   D > 40
+#   Otherwise, F
+#
+# Print the information as follows:
+#
+# LIST OF STUDENTS TAKING ‘Python Programming’ CLASS
+#
+# STUDENT ID	STUDENT NAME	DEPARTMENT 	      RESULT 	  GRADE
+# 123			Biniam		        IT			    62		    C
+# 456			Hareg			    CS			    85		    B
+# 789			Kidu			    IS			    30		    F
